47641e98fb85e3759a7ff0df0dd9db61475ad012
davor <davor@6164e1d0-f123-df11-80f4-0019d13f85ef>
2015-03-20 00:25:25 +0000

    interrupti za emard timer proradili. TODO: brojevi pinova su hardcodirani za logical pinmap
    git-svn-id: svn+ssh://login/fpgasvn/arduino@2578 6164e1d0-f123-df11-80f4-0019d13f85ef

--- hardware/f32c/mips/cores/f32c/WInterrupts.c
+++ hardware/f32c/mips/cores/f32c/WInterrupts.c
@@ -1,83 +1,63 @@
+#include <Arduino.h>
 #include <io.h>
 #include <mips/asm.h>
 #include <mips/cpuregs.h>
 #include <sys/isr.h>
 #include "wiring_private.h"
 
-static volatile voidFuncPtr intFunc[EXTERNAL_NUM_INTERRUPTS] = {NULL, NULL};
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* 8 main MIPS interrupts, all initially disabled */
+static volatile voidFuncPtr intFunc[8] = 
+  { NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, };
+/* some interrupts sources are multiplexed on same mips irq, 
+   here are callbacks for timer */
+static volatile voidFuncPtr timerFunc[4] = {
+    NULL, NULL, // OCP1, OCP2
+    NULL, NULL, // ICP1, ICP2
+};
+
 static int tsc_next;
 int timerInterval = VARIANT_MCK; // default timer interval is 1 second
 
-#if 0
-static int wiring_timer_interrupt(void)
-{
-  if(intFunc[0])
-  {
-    if( (EMARD_TIMER[TC_CONTROL] & (1<<TCTRL_IF_OCP1)) != 0 )
-    {
-      EMARD_TIMER[TC_CONTROL] = ~(1<<TCTRL_IF_OCP1);
-      // EMARD_TIMER[TC_APPLY] = (1<<TC_CONTROL);
-      intFunc[0]();
-    }
-  }
-  if(intFunc[1])
-  {
-    if( (EMARD_TIMER[TC_CONTROL] & (1<<TCTRL_IF_OCP2)) != 0 )
-    {
-      EMARD_TIMER[TC_CONTROL] = ~(1<<TCTRL_IF_OCP2);
-      // EMARD_TIMER[TC_APPLY] = (1<<TC_CONTROL);
-      intFunc[1]();
-    }
-  }
-  return 0;
-}
-
-static struct isr_link timer_interrupt_isr = {
-        .handler_fn = &wiring_timer_interrupt
-};
-
-void enableInterrupt(void)
-{
-  isr_register_handler(VARIANT_TIMER_INTERRUPT, &timer_interrupt_isr);
-  asm("ei");
-}
-
-void attachInterrupt(uint32_t pin, void (*callback)(void), uint32_t mode)
-{
-  if(pin == 14)
-  {
-    intFunc[0] = callback;
-    EMARD_TIMER[TC_CONTROL] |= 
-       (1<<TCTRL_IE_OCP1) | (0<<TCTRL_IE_OCP2)
-     | (0<<TCTRL_IE_ICP1) | (0<<TCTRL_IE_ICP2);
-    EMARD_TIMER[TC_APPLY] = (1<<TC_CONTROL);
-  }
-  if(pin == 15)
-  {
-    intFunc[1] = callback;
-    EMARD_TIMER[TC_CONTROL] |= 
-       (0<<TCTRL_IE_OCP1) | (1<<TCTRL_IE_OCP2)
-     | (0<<TCTRL_IE_ICP1) | (0<<TCTRL_IE_ICP2);
-    EMARD_TIMER[TC_APPLY] = (1<<TC_CONTROL);
-  }
-}
-#endif
-
+/* MIPS timer (inside of the core) */
 static int tsc_isr(void)
 {
-  if(intFunc[0])
+  if(intFunc[7])
   {
-    intFunc[0]();
+    intFunc[7]();
     tsc_next += timerInterval;
     mtc0_macro(tsc_next, MIPS_COP_0_COMPARE);
   }
   return 1;
 }
-
 static struct isr_link tsc_isr_link = {.handler_fn = &tsc_isr};
+
+/* emard advanced timer, memory mapped I/O (outside of the core) */
+static int timer_isr(void)
+{
+  // check hardware registers to determine source of interrupt
+  if( (EMARD_TIMER[TC_CONTROL] & (1<<TCTRL_IF_OCP1)) != 0 )
+  {
+    EMARD_TIMER[TC_CONTROL] &= ~(1<<TCTRL_IF_OCP1); // clear interrupt flag
+    if(timerFunc[0])
+      timerFunc[0]();
+  }
+  if( (EMARD_TIMER[TC_CONTROL] & (1<<TCTRL_IF_OCP2)) != 0 )
+  {
+    EMARD_TIMER[TC_CONTROL] &= ~(1<<TCTRL_IF_OCP2); // clear interrupt flag
+    if(timerFunc[1])
+      timerFunc[1]();
+  }
+  return 1;
+}
+static struct isr_link timer_isr_link = {.handler_fn = &timer_isr};
 
 void attachInterrupt(uint32_t pin, void (*callback)(void), uint32_t mode)
 {
+  int32_t irq = -1;
   /* attachInterrupt is ment to assign pin change interrupt
   ** on digital input pins
   ** but we will here misuse it to create timer interrupt.
@@ -92,16 +72,42 @@ void attachInterrupt(uint32_t pin, void (*callback)(void), uint32_t mode)
   */
   if(pin == 13)
   {
+    uint8_t init_required = 0;
+    irq = 7;
     if(mode)
       timerInterval = mode;
-    if(intFunc[0] == NULL)
+    if(intFunc[irq] == NULL)
+      init_required = 1;
+    intFunc[irq] = callback; // todo - set it above, 
+    if(init_required)
     {
-      isr_register_handler(7, &tsc_isr_link); // 7 is MIPS timer interrput
+      isr_register_handler(irq, &tsc_isr_link); // 7 is MIPS timer interrput
       mfc0_macro(tsc_next, MIPS_COP_0_COUNT);
       tsc_next += timerInterval;
       mtc0_macro(tsc_next, MIPS_COP_0_COMPARE);
     }
-    intFunc[0] = callback;
+    asm("ei");
+  }
+  if(pin == 14 || pin == 15)
+  {
+    irq = 4;
+    if(intFunc[irq] == NULL)
+    {
+      isr_register_handler(irq, &timer_isr_link); // 4 is EMARD timer interrput
+      intFunc[irq] = timer_isr; // not used as callback, just as non-zero to init only once
+    }
+    if(pin == 14)
+    {
+      timerFunc[0] = callback;
+      EMARD_TIMER[TC_CONTROL] |= (1<<TCTRL_IE_OCP1);
+      EMARD_TIMER[TC_APPLY] = (1<<TC_CONTROL);
+    }
+    if(pin == 15)
+    {
+      timerFunc[1] = callback;
+      EMARD_TIMER[TC_CONTROL] |= (1<<TCTRL_IE_OCP2);
+      EMARD_TIMER[TC_APPLY] = (1<<TC_CONTROL);
+    }
     asm("ei");
   }
 }
@@ -109,3 +115,7 @@ void attachInterrupt(uint32_t pin, void (*callback)(void), uint32_t mode)
 void detachInterrupt(uint32_t pin)
 {
 }
+
+#ifdef __cplusplus
+}
+#endif
