bbaddca15cb0e63f4c9f7bb8d88aed78ed559ee3
Emard <vordah@gmail.com>
2015-06-29 00:07:12 +0200

    example for PID

---
+++ libraries/PID/PID.h
@@ -0,0 +1,12 @@
+struct PID_s
+{
+  int sp:24;    unsigned int dummy1:8;
+  int d:6;      unsigned int dummy2:2;
+  int i:6;      unsigned int dummy3:2;
+  int p:6;      unsigned int dummy4:2;
+  unsigned int dummy5:8;
+  int out:12;   unsigned int dummy6:20;
+  int pv:24;    unsigned int dummy7:8;
+};
+
+volatile PID_s *PID = (volatile struct PID_s *) 0xFFFFFD40;
---
+++ libraries/PID/examples/PID/PID.ino
@@ -0,0 +1,60 @@
+#include <PID.h>
+
+void PID_print(int i)
+{
+  char line[255];
+
+  sprintf(line, "PID%d SP=%9d PV=%9d P=%3d I=%3d D=%3d OUT=%5d",
+    i, PID[i].sp, PID[i].pv, PID[i].p, PID[i].i, PID[i].d, PID[i].out
+  ); 
+  Serial.println(line);
+}
+
+void PID_hex(void)
+{
+  char line[255];
+  uint32_t *rp = (uint32_t *) PID;
+  
+  for(int j = 0; j < 4; j++)
+  {
+    sprintf(line, "PID%d %08x",
+      j, rp[j]
+    ); 
+    Serial.println(line);
+  }
+}
+
+void setup() {
+  int i;
+
+  for(i = 0; i < 2; i++)
+  {
+    PID[i].sp = 0;
+    PID[i].p = 8;
+    PID[i].i = 13;
+    PID[i].d = -1;
+  }
+  Serial.begin(115200);
+}
+
+void loop() {
+  int ip;
+  static int sp, state;
+  int phase;
+  int interval = 6;
+  int jump[2] = {100000, -100000};
+
+  for(ip = 0; ip < 2; ip++)
+  {
+    phase = state & ((1<<interval)-1);
+    /*
+    each pid will have different offset
+    */
+    if(phase == 0)
+      PID[ip].sp += (state >> interval) & 1 ? -jump[ip] : jump[ip];
+    if(phase < 8)
+      PID_print(ip);
+  }
+  delay(200);
+  state++;
+}
