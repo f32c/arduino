658082a1d170caa6dedaeac32531a6958bf93212
Emard <vordah@gmail.com>
2015-07-13 23:43:39 +0200

    script to upload boardsmanager files

---
+++ hardware/fpga/f32c/tools/boards-manager/upload.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+scp bm/fpgarduino.zip  login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/bootloaders.zip login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/ujprog-linux32.tar.bz2 login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/ujprog-x86_64-pc-linux-gnu.tar.bz2 login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/ujprog-x86_64-apple-darwin.tar.bz2 login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/ujprog-win32.zip login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/openocd-0.9.0-linux32.tar.bz2 login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/openocd-0.9.0-linux64.tar.bz2 login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/openocd-0.9.0-windows.zip login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/xc3sprog-771-win32.zip login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/xc3sprog-774-linux32.tar.bz2 login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+#scp bm/xc3sprog-774-linux64.tar.bz2 login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/bm/
+scp package_f32c_core_index.json login.nxlab.fer.hr:/jails/www/usr/local/www/data/fpgarduino/
