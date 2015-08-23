bbdca7a907fc0c493f60d92585abd5f40b233318
Emard <vordah@gmail.com>
2015-08-23 11:52:12 +0200

    fprog, usage comments and self-documenting prints

--- libraries/ULX2S/examples/fprog/fprog.ino
+++ libraries/ULX2S/examples/fprog/fprog.ino
@@ -1,3 +1,16 @@
+/*
+         SPI flasher
+         -----------
+
+On fat32 formatted SD card,
+in the root directoy make a
+binary file:
+
+ulx2s_4m.img (length: 4194304 bytes, 4MB)
+
+fprog will copy content of this file
+to oboard SPI flash
+*/
 
 extern "C" {
   #include "fprog.h"
@@ -5,6 +18,9 @@ extern "C" {
 
 void setup(void)
 {
+  Serial.begin(115200);
+  Serial.println("Searching for SD card file ulx2s_4m.img");
+  Serial.println("4194304 bytes will be written to SPI flash");
   fprog();
 }
 
