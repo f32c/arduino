4ae039590361c044bbe5d0ea1a6fe016f954a593
Emard <vordah@gmail.com>
2015-08-23 11:13:54 +0200

    stdio.h: add getchar() putchar()

--- hardware/fpga/f32c/system/include/stdio.h
+++ hardware/fpga/f32c/system/include/stdio.h
@@ -30,6 +30,8 @@
 
 #include <sys/stdint.h>
 
+#define getchar()       sio_getchar(1)
+#define putchar(c)      sio_putchar(c, 1)
 
 int	printf(const char * __restrict, ...) \
 	    __attribute__((format (printf, 1, 2)));
