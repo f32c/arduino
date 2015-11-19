556cae60681af44b73764e20909d0260f46ec0f4
Emard <vordah@gmail.com>
2015-11-19 21:11:08 +0100

    color text example (minimalistic)

--- libraries/f32c_VGATextConsole/examples/text_raw/text_raw.ino
+++ libraries/f32c_VGATextConsole/examples/text_raw/text_raw.ino
@@ -1,21 +1,22 @@
-volatile uint16_t *vram = (volatile uint16_t *) 0x80100000;
-volatile uint32_t *text_addr = (volatile uint32_t *)0xFFFFFB90;
+// crude malloc for video text memory
+// assumes compiled exectuable is <2K
+volatile uint16_t *vram = (volatile uint16_t *) 0x80000800;
+
+#define text_addr (*(volatile uint32_t *)0xFFFFFB90)
 #define cntrl_reg (*(volatile uint8_t *)0xfffffb83)
 
 void setup() {
-  *text_addr = vram; // video text base address
-  cntrl_reg = 0xE0; // enable text mode
+  text_addr = vram; // video text base address
+  cntrl_reg = 0xC0; // enable text mode, no bitmap
 }
 
 void loop() {
-  // display scrolling charse
+  // display scrolling charset
   int i;
-  uint8_t glyph;
-  static uint8_t glyphstart = 0;
-  glyphstart++;
-  glyph = glyphstart;
+  uint16_t glyph;
+  static uint16_t glyphstart = 0;
+  glyph = glyphstart++;
   for(i = 0; i < 80*30; i++)
-    vram[i] = 0x1f00 | (glyph++);
-
+    vram[i] = glyph++;
   delay(100);
 }
