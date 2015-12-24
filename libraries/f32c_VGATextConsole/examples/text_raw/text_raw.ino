ff3a8bcc9c4175650ebd8a6e67ac690ad77c0662
Emard <vordah@gmail.com>
2015-12-24 18:43:42 +0100

    fine scroll to text_raw example

--- libraries/f32c_VGATextConsole/examples/text_raw/text_raw.ino
+++ libraries/f32c_VGATextConsole/examples/text_raw/text_raw.ino
@@ -2,8 +2,10 @@
 // assumes compiled exectuable is <2K
 volatile uint16_t *vram = (volatile uint16_t *) 0x80000800;
 
-#define text_addr (*(volatile uint32_t *)0xFFFFFB90)
+#define text_addr (*(volatile uint32_t *)0xFFFFFB8C)
 #define cntrl_reg (*(volatile uint8_t *)0xfffffb83)
+#define finescroll_reg (*(volatile uint8_t *)0xfffffb8b)
+#define vblank_reg (*(volatile uint8_t *)0xfffffb87)
 
 void setup() {
   text_addr = vram; // video text base address
@@ -15,8 +17,15 @@ void loop() {
   int i;
   uint16_t glyph;
   static uint16_t glyphstart = 0;
-  glyph = glyphstart++;
-  for(i = 0; i < 80*30; i++)
-    vram[i] = glyph++;
-  delay(100);
+  static uint8_t fscroll = 0;
+  while((vblank_reg & 0x80) != 0);
+  while((vblank_reg & 0x80) == 0);
+  fscroll = ++fscroll & 7;
+  finescroll_reg = fscroll;
+  if(fscroll == 0)
+  {
+    glyph = glyphstart++;
+    for(i = 0; i < 81*31; i++)
+      vram[i] = glyph++;
+  }
 }
