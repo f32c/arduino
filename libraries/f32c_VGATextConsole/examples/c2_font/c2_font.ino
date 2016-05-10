525c72672cf3125bb1745178b964fc2be6f03d6c
Emard <vordah@gmail.com>
2016-05-10 14:39:52 +0200

    c2_font add full 26-letter uppercase alphabed 16x16

--- libraries/f32c_VGATextConsole/examples/c2_font/c2_font.ino
+++ libraries/f32c_VGATextConsole/examples/c2_font/c2_font.ino
@@ -7,52 +7,8 @@ extern "C"
   #include "sprite.h"
 }
 
-extern uint8_t content_blue_green[];
-extern uint8_t content_green_blue[];
-extern uint8_t content_reddish[];
-
-volatile struct compositing_line blue_green =
-{
-  NULL, // next line in chain points to previous defined line
-  0, // x-position
-  32, // number of color pixels to follow:
-  content_blue_green,
-};
-
-volatile struct compositing_line green_blue =
-{
-  &blue_green, // last line in chain (has highest priority on display)
-  12, // x-position
-  32, // number of color pixels to follow
-  content_green_blue,
-};
-
-volatile struct compositing_line reddish_line =
-{
-  &green_blue, // links to previous content
-  -2, // x-position
-  8, // number of color pixels to follow:
-  content_reddish,
-};
-
-// similar as above but reverse color order: blue-to-green
-uint8_t content_blue_green[] =
-  {255,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
-
-// some green-to-blue colors
-uint8_t content_green_blue[] =
-  {0xE0,0xE0,0xE0,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,255};
-
-uint8_t content_reddish[] =
-  {0x80,0xE0,0xE0,0xF0,0xF0,0xE0,0xE0,0x80};
-
 volatile struct compositing_line *scanlines[2][480];
-
-struct sprite_speed 
-{
-  int x,y;
-};
-struct sprite_speed Sprite_speed[SPRITE_MAX];
+#define N_LETTERS 27
 
 void setup()
 {
@@ -65,18 +21,16 @@ void setup()
   cntrl_reg = 0b00000000;
   videodisplay_reg = NULL;
   #if 1
-    for(i = 0; i < SPRITE_MAX && i < 4; i++)
+    for(i = 0; i < SPRITE_MAX && i < N_LETTERS; i++)
       shape_to_sprite(i, i);
-    for(i = 3; i < SPRITE_MAX; i++)
-     sprite_clone(i%4, i);
+    for(i = N_LETTERS; i < SPRITE_MAX; i++)
+     sprite_clone(i%N_LETTERS, i);
     // position all sprites in form of a font
     int col=0, row=0;
     for(i = 0; i < SPRITE_MAX; i++)
     {
       Sprite[i]->x = col*16;
       Sprite[i]->y = row*16;
-      Sprite_speed[i].x = 0;
-      Sprite_speed[i].y = 0;
       col++;
       if(col==40)
       {
--- libraries/f32c_VGATextConsole/examples/c2_font/shape.c
+++ libraries/f32c_VGATextConsole/examples/c2_font/shape.c
@@ -16,6 +16,29 @@ struct charcolors std_colors[] =
 };
 
 
+char *shape_slash[] =
+{/*
+ 01234567890123456789012345678901 */
+"WW              ",
+" WW             ",
+"  WW            ",
+"   WW           ",
+"    WW          ",
+"     WW         ",
+"      WW        ",
+"       WW       ",
+"        WW      ",
+"         WW     ",
+"          WW    ",
+"           WW   ",
+"            WW  ",
+"             WW ",
+"              WW",
+"W              W",
+NULL
+};
+
+
 char *shape_a[] =
 {/*
  01234567890123456789012345678901 */
@@ -85,27 +108,524 @@ NULL
 };
 
 
-char *shape_slash[] =
+char *shape_d[] =
 {/*
  01234567890123456789012345678901 */
-"WW              ",
-" WW             ",
-"  WW            ",
-"   WW           ",
-"    WW          ",
-"     WW         ",
-"      WW        ",
-"       WW       ",
-"        WW      ",
-"         WW     ",
-"          WW    ",
-"           WW   ",
-"            WW  ",
-"             WW ",
-"              WW",
-"W              W",
+"                ",
+" CCCCCCCCCCCC   ",
+" CCCCCCCCCCCCC  ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CCCCCCCCCCCCC  ",
+" CCCCCCCCCCCC   ",
+"                ",
 NULL
 };
+
+
+char *shape_e[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GGGGGGGGGGGGGG ",
+" GGGGGGGGGGGGGG ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GGGGGGGGGGG    ",
+" GGGGGGGGGGG    ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GGGGGGGGGGGGGG ",
+" GGGGGGGGGGGGGG ",
+"                ",
+NULL
+};
+
+char *shape_f[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GGGGGGGGGGGGGG ",
+" GGGGGGGGGGGGGG ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GGGGGGGGGGG    ",
+" GGGGGGGGGGG    ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+"                ",
+NULL
+};
+
+
+char *shape_g[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+"   GGGGGGGGGG   ",
+"  GGGGGGGGGGGG  ",
+" GG          GG ",
+" GG          GG ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG      GGGGGG ",
+" GG      GGGGGG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+"  GGGGGGGGGGGG  ",
+"   GGGGGGGGGG   ",
+"                ",
+NULL
+};
+
+
+char *shape_h[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GGGGGGGGGGGGGG ",
+" GGGGGGGGGGGGGG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+"                ",
+NULL
+};
+
+
+char *shape_i[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+"    YYYYYYYY    ",
+"    YYYYYYYY    ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"    YYYYYYYY    ",
+"    YYYYYYYY    ",
+"                ",
+NULL
+};
+
+
+char *shape_j[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+"    GGGGGGGGGGG ",
+"    GGGGGGGGGGG ",
+"             GG ",
+"             GG ",
+"             GG ",
+"             GG ",
+"             GG ",
+"             GG ",
+"             GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+"  GGGGGGGGGGGG  ",
+"   GGGGGGGGGG   ",
+"                ",
+NULL
+};
+
+
+char *shape_k[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG          GG ",
+" GG         GG  ",
+" GG        GG   ",
+" GG       GG    ",
+" GG      GG     ",
+" GG     GG      ",
+" GGGGGGGG       ",
+" GGGGGGGG       ",
+" GG     GG      ",
+" GG      GG     ",
+" GG       GG    ",
+" GG        GG   ",
+" GG         GG  ",
+" GG          GG ",
+"                ",
+NULL
+};
+
+
+char *shape_l[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GG             ",
+" GGGGGGGGGGGGG  ",
+" GGGGGGGGGGGGG  ",
+"                ",
+NULL
+};
+
+char *shape_m[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG          GG ",
+" GGG        GGG ",
+" GGGG      GGGG ",
+" GG GG    GG GG ",
+" GG  GG  GG  GG ",
+" GG   GGGG   GG ",
+" GG    GG    GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+"                ",
+NULL
+};
+
+
+char *shape_n[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG          GG ",
+" GGG         GG ",
+" GGGG        GG ",
+" GG GG       GG ",
+" GG  GG      GG ",
+" GG   GG     GG ",
+" GG    GG    GG ",
+" GG     GG   GG ",
+" GG      GG  GG ",
+" GG       GG GG ",
+" GG        GGGG ",
+" GG         GGG ",
+" GG          GG ",
+" GG          GG ",
+"                ",
+NULL
+};
+
+char *shape_o[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+"   GGGGGGGGGG   ",
+"  GGGGGGGGGGGG  ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+"  GGGGGGGGGGGG  ",
+"   GGGGGGGGGG   ",
+"                ",
+NULL
+};
+
+
+char *shape_p[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" CCCCCCCCCCCC   ",
+" CCCCCCCCCCCCC  ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CCCCCCCCCCCCC  ",
+" CCCCCCCCCCCC   ",
+" CC             ",
+" CC             ",
+" CC             ",
+" CC             ",
+" CC             ",
+" CC             ",
+"                ",
+NULL
+};
+
+
+char *shape_q[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+"   GGGGGGGGGG   ",
+"  GGGGGGGGGGGG  ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG     GG   GG ",
+" GG      GG GG  ",
+" GG       GGG   ",
+" GG       GGG   ",
+"  GGGGGGGGG GG  ",
+"   GGGGGGG   GG ",
+"                ",
+NULL
+};
+
+
+char *shape_r[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" CCCCCCCCCCCC   ",
+" CCCCCCCCCCCCC  ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CC          CC ",
+" CCCCCCCCCCCCC  ",
+" CCCCCCCCCCCC   ",
+" CC     CC      ",
+" CC      CC     ",
+" CC       CC    ",
+" CC        CC   ",
+" CC         CC  ",
+" CC          CC ",
+"                ",
+NULL
+};
+
+char *shape_s[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+"   CCCCCCCCCCCC ",
+"  CCCCCCCCCCCCC ",
+" CC             ",
+" CC             ",
+" CC             ",
+" CC             ",
+"  CCCCCCCCCCC   ",
+"   CCCCCCCCCCC  ",
+"             CC ",
+"             CC ",
+"             CC ",
+"             CC ",
+" CCCCCCCCCCCCC  ",
+" CCCCCCCCCCCC   ",
+"                ",
+NULL
+};
+
+char *shape_t[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" YYYYYYYYYYYYYY ",
+" YYYYYYYYYYYYYY ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"       YY       ",
+"                ",
+NULL
+};
+
+char *shape_u[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+"  GGGGGGGGGGGG  ",
+"   GGGGGGGGGG   ",
+"                ",
+NULL
+};
+
+char *shape_v[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+"  GG        GG  ",
+"   GG      GG   ",
+"    GG    GG    ",
+"     GG  GG     ",
+"      GGGG      ",
+"       GG       ",
+"                ",
+NULL
+};
+
+char *shape_w[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG          GG ",
+" GG    GG    GG ",
+" GG   GGGG   GG ",
+" GG  GG  GG  GG ",
+" GG GG    GG GG ",
+" GGGG      GGGG ",
+" GGG        GGG ",
+" GG          GG ",
+"                ",
+NULL
+};
+
+char *shape_x[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG          GG ",
+"  GG        GG  ",
+"   GG      GG   ",
+"    GG    GG    ",
+"     GG  GG     ",
+"      GGGG      ",
+"       GG       ",
+"       GG       ",
+"      GGGG      ",
+"     GG  GG     ",
+"    GG    GG    ",
+"   GG      GG   ",
+"  GG        GG  ",
+" GG          GG ",
+"                ",
+NULL
+};
+
+
+char *shape_y[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" GG          GG ",
+"  GG        GG  ",
+"   GG      GG   ",
+"    GG    GG    ",
+"     GG  GG     ",
+"      GGGG      ",
+"       GG       ",
+"       GG       ",
+"       GG       ",
+"       GG       ",
+"       GG       ",
+"       GG       ",
+"       GG       ",
+"       GG       ",
+"                ",
+NULL
+};
+
+char *shape_z[] =
+{/*
+ 01234567890123456789012345678901 */
+"                ",
+" CCCCCCCCCCCCCC ",
+" CCCCCCCCCCCCC  ",
+"           CC   ",
+"          CC    ",
+"         CC     ",
+"        CC      ",
+"       CC       ",
+"      CC        ",
+"     CC         ",
+"    CC          ",
+"   CC           ",
+"  CC            ",
+" CCCCCCCCCCCCCC ",
+" CCCCCCCCCCCCCC ",
+"                ",
+NULL
+};
+
 
 struct shape Shape[] =
 {
@@ -113,6 +633,29 @@ struct shape Shape[] =
   [1] = { std_colors, shape_a },
   [2] = { std_colors, shape_b },
   [3] = { std_colors, shape_c },
-  [4] = { NULL, NULL }
+  [4] = { std_colors, shape_d },
+  [5] = { std_colors, shape_e },
+  [6] = { std_colors, shape_f },
+  [7] = { std_colors, shape_g },
+  [8] = { std_colors, shape_h },
+  [9] = { std_colors, shape_i },
+  [10] = { std_colors, shape_j },
+  [11] = { std_colors, shape_k },
+  [12] = { std_colors, shape_l },
+  [13] = { std_colors, shape_m },
+  [14] = { std_colors, shape_n },
+  [15] = { std_colors, shape_o },
+  [16] = { std_colors, shape_p },
+  [17] = { std_colors, shape_q },
+  [18] = { std_colors, shape_r },
+  [19] = { std_colors, shape_s },
+  [20] = { std_colors, shape_t },
+  [21] = { std_colors, shape_u },
+  [22] = { std_colors, shape_v },
+  [23] = { std_colors, shape_w },
+  [24] = { std_colors, shape_x },
+  [25] = { std_colors, shape_y },
+  [26] = { std_colors, shape_z },
+  [27] = { NULL, NULL }
 };
 
