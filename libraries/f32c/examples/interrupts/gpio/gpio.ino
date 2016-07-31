c8d897b85729d9f2adba898c5a88d1f907721188
Emard <vordah@gmail.com>
2016-07-31 19:07:57 +0200

    gpio interrupt example: modified in order to avoid integer multiply in the interrupt handler.

--- libraries/f32c/examples/interrupts/gpio/gpio.ino
+++ libraries/f32c/examples/interrupts/gpio/gpio.ino
@@ -21,20 +21,48 @@
 int output_pin = 32;        // GPIO pin as pulse generator
 int out_indicator_pin = 8;  // LED indicator blinks when pulse is on
 
-int input_pin = output_pin; // software interrupt (no external wiring)
-// int input_pin = 33;      // connect to output_pin with wire
+int input_pin = 33;         // connect to output_pin with wire
+volatile uint32_t *input_pointer; // address of GPIO input MMIO register
+static uint32_t input_bitmask; // GPIO input bit set to 1, other bits 0
+
 int in_indicator_pin =  9;  // LED indicator, if interrputs work it will blink in sync with output pin
+volatile uint32_t *in_indicator_pointer; // address of LED MMIO register
+static uint32_t in_indicator_bitmask; // LED bit set to 1, other bits 0
+
+#define USE_INTERRUPT 1
+#define MANIFEST_MULTIPLY_BUG 0
 
 void gpio_isr(void)
 {
-  digitalWrite(in_indicator_pin, digitalRead(input_pin));
+  //static int toggler;
+  //toggler = ~toggler;
+  //*in_indicator_pointer = toggler;
+  #if MANIFEST_MULTIPLY_BUG
+    digitalWrite(in_indicator_pin, digitalRead(input_pin));
+  #else
+    *in_indicator_pointer =  ((input_bitmask & *input_pointer) == 0)
+                          ?  (*in_indicator_pointer & ~in_indicator_bitmask)
+                          :  (*in_indicator_pointer |  in_indicator_bitmask);
+  #endif
 }
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+
   // initialize digital pin 13 as an output.
   pinMode(output_pin, OUTPUT);
   pinMode(out_indicator_pin, OUTPUT);
+
+  // inside of ISR access LED directly using MMIO register
+  // to avoid interrupt-unfriendly Multiply instruction
+  // probably hidden in digitalRead/digitalWrite
+  input_pointer = portInputRegister(digitalPinToPort(input_pin));
+  input_bitmask = digitalPinToBitMask(input_pin);
+  //delay(1000);
+  //Serial.println((uint32_t) input_pointer, HEX);
+  //Serial.println((uint32_t) input_bitmask, HEX);
+  in_indicator_pointer = portOutputRegister(digitalPinToPort(in_indicator_pin));
+  in_indicator_bitmask = digitalPinToBitMask(in_indicator_pin);
 
   /* in software interrupt mode,
   ** this if clause prevents setting
@@ -48,17 +76,28 @@ void setup() {
   ** both enabled at the same time, with 
   ** same or different interrupt service routine
   */
-  attachInterrupt(input_pin, gpio_isr, RISING);
-  attachInterrupt(input_pin, gpio_isr, FALLING);
+  #if USE_INTERRUPT
+    attachInterrupt(input_pin, gpio_isr, RISING);
+    attachInterrupt(input_pin, gpio_isr, FALLING);
+  #endif
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(output_pin, HIGH);         // turn the signal on (HIGH is the voltage level)
   digitalWrite(out_indicator_pin, HIGH);  // turn the LED on (HIGH)
-  delay(1000);                            // wait for a second
+  #if USE_INTERRUPT
+  #else
+    gpio_isr(); // this will execute with the interrupt instead
+  #endif
+  delay(100);                            // wait for a second
 
   digitalWrite(output_pin, LOW);          // turn the signal off by making the voltage LOW
   digitalWrite(out_indicator_pin, LOW);   // turn the LED off (LOW)
-  delay(1000);                            // wait for a second
+  #if USE_INTERRUPT
+  #else
+    gpio_isr(); // this will execute with the interrupt
+  #endif
+  delay(100);                            // wait for a second
 }
+
